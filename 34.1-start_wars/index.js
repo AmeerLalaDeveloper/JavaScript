80cce9885b5b2910537bcd94917428978daf571a
AmeerLalaDeveloper <laklaw100@gmail.com>
2021-10-05 15:57:31 +0300

    commit

--- 34.1-start_wars/index.js
+++ 34.1-start_wars/index.js
@@ -67,9 +67,13 @@ async function createWarriors(){
 }
 
 (async function(){
- const items=await Promise.all(await createWarriors())
+ const items=
+ await Promise.all(await createWarriors())
+  
   createTable(items)
+  //createTable2(items)
 })();
+
 function createTable(warriors){
     
     let table=
@@ -82,7 +86,6 @@ function createTable(warriors){
     <th>planet population</th>
   </tr>`
   for(let warrior of warriors){
-
    table+=
    `<tr>
    <td>${warrior.name}</td>
@@ -93,8 +96,43 @@ function createTable(warriors){
    </tr>`
 
   }
-
   table+='</table>'
-
   document.body.innerHTML=table;
 }
+
+
+// function createTable2(warriors){
+//   let keys=['name','hair','height','planet name','planet population']
+//   let table=document.createElement('table');
+//   table.style.width='100%';
+//   let row=document.createElement('tr');
+//   for(let i=0;i<5;i++)
+//   {
+//     let head=document.createElement('th')
+//     head.textContent=keys[i]
+//     row.appendChild(head)
+//   }
+//   table.appendChild(row)
+//   for(let warrior of warriors){
+//    let tr=document.createElement('tr');
+//    let td1=document.createElement('td')
+//    td1.textContent=warrior.name;
+//    let td2=document.createElement('td')
+//    td2.textContent=warrior.hair;
+//    let td3=document.createElement('td')
+//    td3.textContent=warrior.height;
+//    let td4=document.createElement('td')
+//    td4.textContent=warrior.planet.name;
+//    let td5=document.createElement('td')
+//    td5.textContent=warrior.planet.population;
+//   tr.appendChild(td1)
+//   tr.appendChild(td2)
+//   tr.appendChild(td3)
+//   tr.appendChild(td4)
+//   tr.appendChild(td5)
+//   table.appendChild(tr)
+//   }
+
+//   document.body.appendChild(table);
+
+// }
